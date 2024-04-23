dde3e0dad286f37301327be05ae6a9c1b0cd4bb0
ClaudiuLBS <clau_123@yahoo.com>
2024-04-23 21:09:49 +0300

    configure dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const SEPOLIA_URL = process.env.SEPOLIA_URL
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+        url: SEPOLIA_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
